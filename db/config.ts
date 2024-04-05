acf4c34ab3bd49e318f3d5318b0f5a201d5b9866
Anthony Mifsud <amifsud02@gmail.com>
2024-04-06 00:50:17 +0200

    chore: update images and layout shift

--- db/config.ts
+++ db/config.ts
@@ -4,19 +4,24 @@ const CS2Leaderboard = defineTable({
   columns: {
     // User
     id: column.text({ primaryKey: true }), // faceit id
-    username: column.text(),
+    nickname: column.text(),
     profileLink: column.text(), // might not need this
     // Stats
-    elo: column.number(),
+    position: column.number(),
+    faceit_elo: column.number(),
+    game_skill_level: column.number(),
     movement: column.number(), // 0 = no change, 1 = up, -1 = down
     // Metadata
     createdAt: column.date({ default: NOW }),
-    updatedAt: column.date(),
-    deletedAt: column.date()
+    updatedAt: column.date({ optional: true }),
+    deletedAt: column.date({ optional: true }),
+
   },
   indexes: {
     user_idx: { on: ["id"], unique: true }
-  }
+  },
+
+  
 })
 
 export default defineDb({
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,15 @@
-import { db } from 'astro:db';
+import { db, CS2Leaderboard } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+	// await db.insert(CS2Leaderboard).values([
+	// 	{
+	// 		id: '1234567890',
+	// 		username: 'username',
+	// 		elo: 1000,
+	// 		movement: 1,
+	// 		profileLink: 'https://www.faceit.com/en/players/username',
+	// 		createdAt: new Date(),
+	// 	}
+	// ])
 }
